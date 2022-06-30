abf25558a0a141b6b6e99582ef19c226cce0f845
mahdiimeni <mahdi.imeni@gmail.com>
2022-06-30 13:58:44 +0430

    # Some comments added

--- main.py
+++ main.py
@@ -2,9 +2,15 @@
 
 from calc_brain import Calc
 
+# --- Percentage of desired daily profit.
 percent = float(input("% > "))
+
+# --- Number of working days.
 trade_days = int(input("Days > "))
+
+# --- The first amount of the transaction.
 initial_fund = float(input("$ > "))
+
 choice = input("Compound profit or Fix profit? (C or F): ").lower()
 
 calculator = Calc(percent, trade_days, initial_fund)
